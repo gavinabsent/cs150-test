f7a3f4adf94e51773f331fff80e90f2005d5d274
gavinabsent <grstarr@students.nic.edu>
2022-10-28 10:50:54 -0700

    Cause conflic

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main()
 {
     cout << "Hello, world!" << endl;
     cout << "This is another edit." << endl;
+    cout << "Uh oh, this local change isn't consistent with the global repository" << endl;
     return 0;
 }
