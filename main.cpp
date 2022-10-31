911bf7a7ab1b02b3ebcbee3440393a43d6bb2ffd
gavinabsent <grstarr@students.nic.edu>
2022-10-31 10:14:40 -0700

    Add cin statement

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 
 using std::cout;
 using std::endl;
+using std::cin;
 
 int main() 
 {
     cout << "Hello, world!" << endl;
     cout << "This is another edit." << endl;
     cout << "Adding this line creates a conflict" << endl;
-    cout << "Uh oh, this local change isn't consistent with the global repository" << endl;
+
+
+    int num;
+    cin >> num;
+    cout << "You entered: " << num << endl;
     return 0;
 }
